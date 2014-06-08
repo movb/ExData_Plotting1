58545c14b0e87b63379c1bb79e73981368ab276f
Mikhail Plekhanov <mike.plekhanov@gmail.com>
2014-06-08 22:37:55 +0000

    Added Plot1 files.

---
+++ plot1.R
@@ -0,0 +1,13 @@
+idata <- read.table("household_power_consumption.txt", header=TRUE, sep=";", stringsAsFactors=FALSE)
+
+data$Date <- as.Date(data$Date, format="%d/%m/%Y")
+
+data <- subset(data, data$Date == as.Date("2007/02/01") | data$Date == as.Date("2007/02/02"))
+
+data$Global_active_power <- as.numeric(data$Global_active_power)
+
+hist(data$Global_active_power, col="red", 
+          main="Global Active Power",xlab="Global Active Power(kilowatts)", ylab="Frequency")
+
+dev.copy(png, file="Plot1.png", width=480, height=480)
+dev.off()
